8d3b4f6373e731654814609e7d74ad777f24ee6b
TobyDevOps <Tngoetz@aol.com>
2024-03-14 02:57:14 -0400

    Updated Pear Standards for Reservations.php class

--- classes/Reservations.php
+++ classes/Reservations.php
@@ -17,11 +17,11 @@ class Reservations
 
 
     /**
-     * @param string $_firstname
-     * @param string $_lastName
-     * @param string $_email
-     * @param $_date
-     * @param $_time
+     * @param string $_firstname first name of client
+     * @param string $_lastName last name of client
+     * @param string $_email email of client
+     * @param DateTime $_date  date of clients reservation
+     * @param DateTime $_time time of clients reservation
      */
     public function __construct(
         $_firstname,
@@ -37,18 +37,24 @@ class Reservations
         $this->_time = $_time;
     }
 
+    /**
+     * @return string clients first name
+     */
     public function getFirstname(): string
     {
         return $this->_firstname;
     }
 
+    /**
+     * @param string $firstname clients first name
+     */
     public function setFirstname(string $firstname): void
     {
         $this->_firstname = $firstname;
     }
 
     /**
-     * @return mixed
+     * @return string clients last name
      */
     public function getLastName()
     {
@@ -56,7 +62,7 @@ class Reservations
     }
 
     /**
-     * @param mixed $lastName
+     * @param string $lastName clients last name
      */
     public function setLastName($lastName): void
     {
@@ -64,7 +70,7 @@ class Reservations
     }
 
     /**
-     * @return mixed
+     * @return string clients email
      */
     public function getEmail()
     {
@@ -72,7 +78,7 @@ class Reservations
     }
 
     /**
-     * @param mixed $email
+     * @param string $email clients email
      */
     public function setEmail($email): void
     {
@@ -80,7 +86,7 @@ class Reservations
     }
 
     /**
-     * @return mixed
+     * @return mixed total calculated price for reservation
      */
     public function getTotalPrice()
     {
@@ -88,28 +94,42 @@ class Reservations
     }
 
     /**
-     * @param mixed $totalPrice
+     * @param mixed $totalPrice total calculated price for reservation
      */
     public function setTotalPrice($totalPrice): void
     {
         $this->_totalPrice = $totalPrice;
     }
 
+    /**
+     * @return mixed date of reservation
+     */
     public function getDate()
     {
         return $this->_date;
     }
 
+    /**
+     * @param DateTime $date date of reservation
+     * @return void
+     */
     public function setDate($date)
     {
         $this->_date = $date;
     }
 
+    /**
+     * @return mixed time of reservation
+     */
     public function getTime()
     {
         return $this->_time;
     }
 
+    /**
+     * @param DateTime $time time of reservation
+     * @return void
+     */
     public function setTime($time)
     {
         $this->_time = $time;
