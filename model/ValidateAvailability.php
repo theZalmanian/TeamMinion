7ca966eb7f0d8f55f9cfccafa3d4b7804813afba
theZalmanian <syntaxwarden@protonmail.com>
2024-03-13 21:05:58 -0400

    Refatored ValidateAvailability and added documentation

--- model/ValidateAvailability.php
+++ model/ValidateAvailability.php
@@ -3,41 +3,45 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// access DB connection constants
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config.php');
 
 /**
- * Validate Object created to fun validation methods on user Input
- * and contains connection to database via field
+ * Contains various methods used to interact with the reservation DB
  */
 class ValidateAvailability
 {
+    /**
+     * @var PDO PDO Connection to DB
+     */
     private $_dbh;
 
     /**
-     * constructs a ValidateAvailability object
+     * Attempts to establish PDO connection to DB
      */
     public function __construct()
     {
+        // attempt to connect to DB
         try {
             // Instantiate a PDO database connection object
             $this->_dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-            echo 'Connected to database!';
-        } catch (PDOException $e) {
-            echo $e->getMessage(); #temporary
+        }
+
+        // display error if connection failed
+        catch (PDOException $exception) {
+            echo $exception->getMessage();
         }
     }
 
     /**
-     * Collects the current date clicked from calendar and queries
-     * database to render available time slots for massage
-     * @param $currDate
-     * @return void
+     * Generates a button for each hour of the given date which has not been currently reserved for a massage
+     * @param string $currDate The date being searched in DB
      */
     function getAvailableTimes($currDate)
     {
-        // run through all time slots (09:00 - 17:00), and only display times not reserved in DB
+        // run through all working hours (09:00 - 17:00), and only display times not reserved in DB
         for ($currHour = 9; $currHour <= 17; $currHour++) {
-            // check db for a reservation at the current date selected through calendar and hour
+            // check db for a reservation at the given date and time
             $query = "SELECT * 
                       FROM massages
                       WHERE massageDate = :currDate AND massageTime = :currHour";
@@ -65,35 +69,41 @@ class ValidateAvailability
                 $timeSuffix = $currHour < 12 ? 'am' : 'pm';
 
                 // display a button for that hour
-                echo "<button class='timeSlots btn btn-primary' id='{$displayHour}' value='{$currHour}:00:00'>{$displayHour}:00 {$timeSuffix}</button>";
+                echo "<button class='timeSlots btn btn-primary' id='{$displayHour}' value='{$currHour}:00:00'>
+                            {$displayHour}:00 {$timeSuffix}
+                        </button>";
             }
         }
     }
 
     /**
-     * Gets the current reservation object from session and saves it to DB
+     * Gets the current massage reservation from session (if exists), and adds it to the DB
      */
     function reserveMassage() {
-        // grab current reservation from session
+        // grab current massage reservation from session
         $currReservation = $_SESSION["currMassageReservation"];
 
-        $query = "INSERT INTO massages (massageDate, massageTime, massageType, hotStones, massageIntensity, firstName, lastName, email) 
-                    VALUES (:massageDate, :massageTime, :massageType, :hotStones, :massageIntensity, :firstName, :lastName, :email)";
+        // id one exists, attempt to insert it into the DB
+        if(!empty($currReservation)) {
+            // setup query
+            $query = "INSERT INTO massages (massageDate, massageTime, massageType, hotStones, massageIntensity, firstName, lastName, email) 
+                        VALUES (:massageDate, :massageTime, :massageType, :hotStones, :massageIntensity, :firstName, :lastName, :email)";
 
-        // prepare the statement
-        $statement = $this->_dbh->prepare($query);
+            // prepare the statement
+            $statement = $this->_dbh->prepare($query);
 
-        // bind parameters
-        $statement->bindValue(":massageDate", $currReservation->getDate());
-        $statement->bindValue(":massageTime", $currReservation->getTime());
-        $statement->bindValue(":massageType", $currReservation->getMassageType());
-        $statement->bindValue(":hotStones", $currReservation->isHotStone());
-        $statement->bindValue(":massageIntensity", $currReservation->getIntensity());
-        $statement->bindValue(":firstName", $currReservation->getFirstName());
-        $statement->bindValue(":lastName", $currReservation->getLastName());
-        $statement->bindValue(":email", $currReservation->getEmail());
+            // bind parameters from current submitted reservation
+            $statement->bindValue(":massageDate", $currReservation->getDate());
+            $statement->bindValue(":massageTime", $currReservation->getTime());
+            $statement->bindValue(":massageType", $currReservation->getMassageType());
+            $statement->bindValue(":hotStones", $currReservation->isHotStone());
+            $statement->bindValue(":massageIntensity", $currReservation->getIntensity());
+            $statement->bindValue(":firstName", $currReservation->getFirstName());
+            $statement->bindValue(":lastName", $currReservation->getLastName());
+            $statement->bindValue(":email", $currReservation->getEmail());
 
-        // execute the query
-        $statement->execute();
+            // execute the query
+            $statement->execute();
+        }
     }
 }
